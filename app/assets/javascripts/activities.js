c0b6d3c0584aefc5ea5b4138cd902cf13421ebba
smarsh25 <sjm.atl@gmail.com>
2014-02-22 12:27:03 -0800

    abstracting display activites

--- app/assets/javascripts/activities.js
+++ app/assets/javascripts/activities.js
@@ -7,19 +7,14 @@ $(function() {
   };
 
   Activities.displayActivities = function(responseData) {
-    $(responseData).each(function(index, activity) {
-      $("#activities").append("<a href='/'>" + activity.title + "</a>");
-    });
-  };
-
-  Activities.getItems = function() {
-    //$.get(this.urls.index.path).done(this.displayActivities(responseData));
-    $.get(this.urls.index.path).done(function(responseData) {
       $(responseData).each(function(index, activity) {
         var activityHTML = HandlebarsTemplates.activities(activity);
         $("#activities").append(activityHTML);
       });
-    });
+  };
+
+  Activities.getItems = function() {
+    $.get(this.urls.index.path).done(this.displayActivities);
   };
 
   Activities.getItems();
