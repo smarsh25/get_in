d032762f6881fba74e00b4c3427b0af3acd33187
Whit Schrader <whit@schraderlabs.com>
2014-02-21 21:16:19 -0800

    can add users to the db with devise, no pwd reminder

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+   include SessionsHelper
+
+  def new
+  end
+
+  def create #finding an existing user, making a new session instatiating them
+    @user = User.find_by_email(params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      # Sign the user in and redirect to the user's show page.
+      sign_in @user
+      redirect_to search_path
+    else
+      # Create an error message and re-render the signin form.
+      flash.now[:error]='Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_url
+  end
+
+end
