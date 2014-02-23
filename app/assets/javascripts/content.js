800aa5750347de1319c2f5f4ed41f62068de728c
Tanner Swenson <TannerSwenson@gmail.com>
2014-02-23 10:15:13 -0800

    Added comments

--- app/assets/javascripts/content.js
+++ app/assets/javascripts/content.js
@@ -4,13 +4,13 @@ $(function(){
   content_index  : { url : '/activities/', url2 : '/content.json', method : 'get' },
   create_content : { url : '/activities/', url2 : '/content.json', method : 'post'}
   };
-
+  //renders a givin content object
   Content.render_content = function(content) {
     content = content;
     html = HandlebarsTemplates.content(content);
     $('#content').append(html);
   };
-
+  //gets and arrary of content objects from the server
   Content.get_content = function(activity_id, callback){
     var _this = this;
     $.ajax({
@@ -20,7 +20,7 @@ $(function(){
       callback(data);
     });
   };
-
+  //sends a post request to the server to add content to the activity
   Content.create_content = function(activity_id, content){
     var _this = this;
     $.ajax({
@@ -31,12 +31,12 @@ $(function(){
       _this.render_content(data.content);
     });
   };
-
+  //runs a function within the Content scope
   Content.do_this = function(fn){
         fn.apply(Content);
         return this;
   };
-
+  //when user submits form gets title of content cleare the field and calls create content
   Content.do_this(function(){
     $('#add_activity_content').on('submit', function(event){
       event.preventDefault();
@@ -47,6 +47,7 @@ $(function(){
       Content.create_content(activity_id, content);
     });
   });
+  //gets all the content and renders it on the page
   Content.get_content($('#content').attr('activity-id'), function(data){
     $(data.contents).each(function(index, item){
         Content.render_content(item);
